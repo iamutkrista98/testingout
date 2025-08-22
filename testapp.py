5dd495c9378a4ac02fcdc2813817112ce0d983d5
Utkrista Acharya <66366728+iamutkrista98@users.noreply.github.com>
2025-08-22 09:46:20 +0545

    Update testapp.py

--- testapp.py
+++ testapp.py
@@ -79,10 +79,10 @@ def preprocess_image_grayscale(uploaded_file):
 
 # ---------------- UI INPUTS ----------------
 model_url = st.text_input("🔗 Paste public Google Drive link to .h5 model file", 
-    value="https://drive.google.com/file/d/1GN6B1Kpi3M8KYGm1CqWNxwqTs6nU51u8")
+    value="https://drive.google.com/uc?id=1GN6B1Kpi3M8KYGm1CqWNxwqTs6nU51u8")
 
 excel_url = st.text_input("🔗 Paste public Google Drive link to Excel file (.xlsx)", 
-    value="https://drive.google.com/file/d/1dJbbLx348xTBiOCh4ywW-qAcfNhqbrVO")
+    value="https://drive.google.com/uc?id=1dJbbLx348xTBiOCh4ywW-qAcfNhqbrVO")
 
 uploaded_file = st.file_uploader("📤 Upload Leaf Image", type=["jpg", "jpeg", "png"])
 
